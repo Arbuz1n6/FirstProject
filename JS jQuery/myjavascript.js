5ed2b8394b78009d324fa5c20ffe1c3aca26f25a
Arbuz1n6 <57681166+Arbuz1n6@users.noreply.github.com>
2021-08-12 16:10:27 +0200

    Add files via upload

---
+++ JS jQuery/myjavascript.js
@@ -0,0 +1,44 @@
+//let one = 'Моя первая переменная';
+
+//alert(one);
+//console.log(one);
+//document.getElementById('test').innerHTML = one;
+//$('#test').text(one);
+//$('#test').fadeOut("slow");
+
+//$('.myclass').text(one);
+
+
+//document.getElementsByClassName("myclass")[0].innerHTML = one;
+//console.log(document.getElementsByClassName("myclass"));
+
+//document.getElementsByClassName("myclass")[0].style.color = "red";
+//$('.myclass').css('color', 'green');
+//$('.myclass').css('background-color', 'blue');
+
+//array = [1, 2, 34, 5];
+//for (i = 0; i < array.length; i++) {
+//    console.log(array.length);}
+
+//function mysum(a, b) {
+//    return a + b;
+//}
+//console.log(mysum(3, 5));
+
+
+
+//function mySumShow(a, b) {
+//result = a + b;
+// document.getElementsByClassName("myclass")[0].innerHTML = mysum(a, b);
+
+//}
+//mySumShow(3, 666);
+
+//test.onclick = function () {
+//   alert(' Опа чирик!');
+//}
+var say = function () {
+    alert(' Опа чирик!');
+}
+test.addEventListener('mouseover', say);
+//test.removeEventListener('click', say);
